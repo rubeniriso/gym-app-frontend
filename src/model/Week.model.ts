55e42c1a48a811c5818e88af926b3563aff25cbe
eugen hamuraru <eugenhamuraru@Air-de-eugen.technicolor.net>
2024-04-14 14:16:12 +0200

    Routine weeks fixes

--- src/model/Week.model.ts
+++ src/model/Week.model.ts
@@ -1,11 +1,12 @@
-'use server'
-async function getAllRotuineWeeks(routineId: number) {
+async function getAllRotuineWeeks(routineId: string) {
   try {
+    console.log(routineId)
     const response = await fetch(
       `${process.env.DOMAIN_URL}/api/v1/weeks/routine/${routineId}`,
       { next: { tags: ["weeks"] }, method: "GET" }
     );
     const data = await response.json();
+    console.log(data)
     return data;
   } catch (error) {
     console.error("Error fetching session data:", error);
